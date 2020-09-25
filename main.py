93460307daccf7359e1720c6fceaff6af81a6d91
Noah <noahhodge2@gmail.com>
2020-09-24 18:40:50 -0700

    rpg class

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import random
 
 class Character:
 
-    def __init__(self, name, gender):
+    def __init__(self, name, gender, help):
         self.name = name
         self.gender = gender
         self.gold = random.randint(5,150000)
+        self.help = help
