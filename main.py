3e8089c452c82bdd7770fc46bc2e39a90cd5e4ed
Noah <noahhodge2@gmail.com>
2020-09-26 16:26:30 -0700

    rpg class

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import random
 
 class Character:
 
-    def __init__(self, name, gender, help):
+    def __init__(self, name, gender, vocation):
         self.name = name
         self.gender = gender
+        self.vocation = vocation
         self.gold = random.randint(5,150000)
+
+    def __str__(self):
+        return f"{self.name} the {self.vocation}"
 
