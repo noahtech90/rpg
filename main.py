a3ea03e8d1ffbafb1a00c035754fcdd30b41e26b
Noah <noahhodge2@gmail.com>
2020-09-26 16:47:57 -0700

    rpg class tests

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
-
+import time
 
 class Character:
+    character_stats = dict(health=100, mana=50, speed=10, luck=1)
 
     def __init__(self, name, gender, vocation):
         self.name = name
@@ -10,5 +11,56 @@ class Character:
         self.gold = random.randint(5,150000)
 
     def __str__(self):
-        return f"{self.name} the {self.vocation}"
+        capitalized_vocation = self.vocation[0].upper() + self.vocation[1:]
+        return f"   {self.name} the {capitalized_vocation}"
 
+    def pronoun(self):
+        if self.gender == 'male':
+            return "he"
+        elif self.gender == 'female':
+            return "she"
+        else:
+            return "they"
+
+    def possesive(self):
+        if self.gender == 'male':
+            return "his"
+        elif self.gender == 'female':
+            return "her"
+        else:
+            return "their"
+
+    def weapon(self):
+
+        if self.vocation == 'wizard':
+            return "staff"
+        elif self.vocation == 'warrior':
+            return "sword"
+        elif self.vocation == 'archer':
+            return "bow"
+        else:
+            return "bear"
+
+print("""
+
+    Welcome to a very generic rpg game
+
+    """)
+
+raw_name = input("What is your character's name?")
+name = raw_name[0].upper() + raw_name[1:].lower()
+time.sleep(2)
+print("What gender is your character?")
+gender = input("Male                Female                Other").lower()
+time.sleep(2)
+print("""
+    What class is your character?
+    """)
+vocation = input("      Wizard            Warrior           Archer").lower()
+print("")
+time.sleep(2)
+character = Character(name, gender, vocation)
+print(f"{character} embarked on {character.possesive()} quest.  {character.name} knew only that there was a battle coming.")
+time.sleep(2)
+print("")
+print(f"\t{character} drew {character.possesive()} {character.weapon()} and headed down the road")
