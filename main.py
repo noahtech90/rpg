b71df6d780635be176c1d6852be3ceae9b127a8f
Noah <noahhodge2@gmail.com>
2020-10-02 19:20:13 -0700

    rpg documentation

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import random
 import time
 
+"""
+Character class which creates the story 
+
+Character can attack and defend, similar to a finial fantasy type game
+
+"""
+
+
 class Character:
     character_stats = dict(health=100, mana=50, speed=10, luck=1)
 
@@ -8,7 +16,7 @@ class Character:
         self.name = name
         self.gender = gender
         self.vocation = vocation
-        self.gold = random.randint(5,150000)
+        self.gold = random.randint(5, 150000)
 
     def __str__(self):
         capitalized_vocation = self.vocation[0].upper() + self.vocation[1:]
@@ -41,31 +49,39 @@ class Character:
         else:
             return "bear"
 
+    def attack(self):
+        attack_damage = random.randint(15, 30)
+        return attack_damage
+
+
+# Intro
 print("""You wake up in a dark cave""")
 print("")
 time.sleep(2)
-
-raw_name = input("What is your character's name? ")
+# Assign Name
+raw_name = input('What is your character\'s name? ')
 name = raw_name[0].upper() + raw_name[1:].lower()
 time.sleep(2)
-
+# Assign Gender
 print("What gender is your character? ")
 gender = input("Male                Female                Other").lower()
 time.sleep(2)
-
+# Assign Player Type
 print("""What class is your character? """)
-vocation = input("      Wizard            Warrior           Archer").lower()
+vocation = input('      Wizard            Warrior           Archer').lower()
 print("")
 time.sleep(2)
 
 character = Character(name, gender, vocation)
 
+
 def start_game(character):
-    print(f"{character} embarked on {character.possesive()} quest.  {character.name} knew only that there was a battle coming.")
+    print(
+        f"{character} embarked on {character.possesive()} quest.  {character.name} knew only that there was a battle coming.")
     time.sleep(2)
     print("")
     print(f"\t{character} drew {character.possesive()} {character.weapon()} and headed down the road")
-
+    time.sleep(2)
 
 
 start_game(character)
