561c50345e7d5d3a9f8f2501935c4c6aa55e3d58
Noah <noahhodge2@gmail.com>
2020-10-04 15:36:06 -0700

    Battle Scenario

--- character.py
+++ character.py
@@ -2,7 +2,7 @@ import random
 import time
 
 class Character:
-    character_stats = dict(health=100, mana=50, speed=10, luck=1)
+    character_stats = dict(health=120, mana=50, speed=10, luck=1.3)
 
     def __init__(self, name, gender, vocation):
         self.name = name
@@ -42,16 +42,20 @@ class Character:
             return "bear"
 
     def attack(self):
-        attack_damage = random.randint(15, 30) + (self.character_stats['luck'] * 1.2)
+        attack_damage = random.randint(15, 50) + (self.character_stats['luck'] * 1.2)
         return attack_damage
 
 class Enemy(Character):
-
+    character_stats = dict(health=70, mana=20, speed=5, luck=1)
 
     def __init__(self, name_list, genders, classes):
         self.name = name_list.pop(random.randint(0, len(name_list)) - 1)
         self.gender = genders[random.randint(0,2)]
         self.vocation = classes[random.randint(0,2)]
+
+    def attack(self):
+        attack_damage = random.randint(5, 40)
+        return attack_damage
 
 
 
--- main.py
+++ main.py
@@ -24,33 +24,68 @@ print("")
 time.sleep(2)
 # Assign Name
 raw_name = input('What is your character\'s name? ')
+#raw_name = "Noah"
 name = raw_name[0].upper() + raw_name[1:].lower()
 time.sleep(2)
 print("")
+
 # Assign Gender
 print("What gender is your character? ")
 time.sleep(1)
 gender = input("Male                Female                Other ").lower()
+#gender = 'male'
 time.sleep(2)
 print("")
 
 # Assign Player Type
 print("""What class is your character? """)
 time.sleep(1)
-vocation = input('      Wizard            Warrior           Archer ').lower()
+vocation = input('Wizard            Warrior           Archer ').lower()
+
+#vocation = 'wizard'
 print("")
 time.sleep(2)
 
 character = Character(name, gender, vocation)
 
 
-def start_game(character):
-    print(f"{character} woke in a dark mysterious cave.  {character.name} knew not where {character.pronoun()} came from, nor what {character.pronoun()} was doing in this desolate place. ")
+def start_game(main_character):
+    print(f"{main_character} woke in a dark mysterious cave.  {main_character.name} knew not where {main_character.pronoun()} came from, nor what {main_character.pronoun()} was doing in this desolate place. ")
+    time.sleep(3)
+    print("")
+    print(f"{main_character.name} drew {main_character.possesive()} {main_character.weapon()} and moved towards a "
+          f"faint dripping sound")
     time.sleep(2)
     print("")
-    print(f"{character.name} drew {character.possesive()} {character.weapon()} and moved towards a faint dripping sound")
-    time.sleep(2)
-    print(f"Suddenly {character.name} is attacked")
+    print(f"Suddenly, a snarling hideous creature clutching its {first_enemy.weapon()} jumped out, {first_enemy} lunges "
+          f"towards {main_character.name}")
+    first_battle(main_character)
+
+def first_battle(main_character):
+    while main_character.character_stats['health'] > 0 and first_enemy.character_stats['health'] > 0:
+        decision = input('Attack         Counter          Heal').lower()
+        if decision == 'attack':
+            first_enemy.character_stats['health'] -= main_character.attack()
+        elif decision == 'counter':
+            attempt = first_enemy.character_stats['luck'] * random.randint(1, 5)
+            if attempt > 4:
+                first_enemy.character_stats['health'] -= 100
+        elif decision == 'heal':
+            main_character.character_stats['health'] += 10
+        main_character.character_stats['health'] -= first_enemy.attack()
+
+        print("")
+        print("")
+        time.sleep(1)
+        print(f"{main_character}: " + str(main_character.character_stats))
+        print("")
+        print(f"{first_enemy}: " + str(first_enemy.character_stats))
+        print("")
+        print("")
+        time.sleep(2)
+
+
+
 
 #Create Enemies
 first_enemy = Enemy(enemy_available, genders, classes)
@@ -58,6 +93,6 @@ second_enemy = Enemy(enemy_available, genders, classes)
 third_enemy = Enemy(enemy_available, genders, classes)
 fourth_enemy = Enemy(enemy_available, genders, classes)
 
-
+#Initiate Story
 start_game(character)
-print(first_enemy)
+
