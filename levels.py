befd9e573eb04fee5888ac314922001215461b1e
Noah <noahhodge2@gmail.com>
2020-10-07 18:55:48 -0700

    Adding Leveling System

--- levels.py
+++ levels.py
@@ -4,22 +4,27 @@ List of levels for Character to Travel Through
 Player location determines surrounding sotry
 
 """
+import json
 
-class Level(object):
-    def __init__(self, player):
-        self.player = player
-        self.next = None
-        self.backwards = None
-
-class DoublyLinkedLevelList(object):
-    def __init__(self):
-        self.start_level = None
-
-
-
-list = LinkedList()
-player_one = Player(1)
+def index_to_level(player_index):
+    index_to_level_descript = {
+        1: 'level_one',
+        2: 'level_two',
+        3: 'level_three',
+        4: 'level_four',
+    }
+    return index_to_level[player_index]
 
 
-print(list)
+def establish_location(level):
+
+    with open('locations.json') as f:
+        story = json.load(f)
+        return story['locations'][level]
+
+
+
+
+
+
 
